218de8cd00b0bbf9bc9f6c9a77b8be26f41ccf16
wealthybeans <wealthybeans@wealthybeanss-MacBook-Air.local>
2023-06-12 12:22:42 -0700

    add cronos chain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,40 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { Chain, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { arbitrum, goerli, mainnet, optimism, polygon } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
+import React from 'react';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+
+
+const cronosChain: Chain = {
+  id: 25,
+  name: 'Cronos',
+  network: 'mainnet',
+  
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Cronos',
+    symbol: 'CRO',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://evm.cronos.org'],
+    },
+    public: {
+      http: ['https://evm.cronos.org'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+    etherscan: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+  },
+  testnet: false,
+};
+
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
@@ -12,14 +42,18 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
     polygon,
     optimism,
     arbitrum,
+    cronosChain,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
   ],
-  [publicProvider()]
+  [publicProvider(),
+    jsonRpcProvider({
+      rpc: chain => ({ http: chain.rpcUrls.default.http[0] }),
+    }),]
 );
 
 const { connectors } = getDefaultWallets({
   appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  projectId: '3143e4bb9225c0d9693cc7db0a82325b',
   chains,
 });
 
